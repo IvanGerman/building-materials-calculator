03ecfd4315652f6280d0e9c474bd93fed2d6c121
IvanGerman <ivgvi@mail.ru>
2023-07-24 15:40:18 +0700

    feat: implementing simple logic for counting the volume of the order (in m3)

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,18 @@ function App() {
 
   useEffect(() => {
     const selectedBoard: HTMLElement = document.querySelector('.selected-item-img') as HTMLElement;
-    console.log(selectedBoard);
+
+    // taking data from input/select elements
+    const quantityInput: HTMLInputElement = document.querySelector('#quantity') as HTMLInputElement;
+    
+    const widthSelect: HTMLInputElement = document.querySelector('#width') as HTMLInputElement;
+    
+    const lengthSelect: HTMLInputElement = document.querySelector('#length') as HTMLInputElement;
+    
+    
+
     selectedBoard.ondragstart = function callback(event) {
-      console.log('selectedBoard.ondragstart');
+      console.log('selectedBoard.ondragstart'); 
     };
 
       function handleOver(event: Event) {
@@ -31,9 +40,17 @@ function App() {
       function handleDrop(event: Event) {
         event.preventDefault();
         console.log('handleDrop');
+        let quantity = quantityInput.value;
+        let width = widthSelect.value;
+        let length = lengthSelect.value;
+        console.log(quantity);
+        console.log(width);
+        console.log(length);
+        let volume = ((Number(length) * 1000) * Number(width) * 25 * Number(quantity)) / 1000000000;
+        console.log('Объём заказа в кубических метрах = ', volume);
       }
     
-      const basketTargetZone: HTMLElement = document.querySelector('.basket') as HTMLElement;
+      const basketTargetZone: HTMLElement = document.querySelector('.basketTargetZone') as HTMLElement;
     
       basketTargetZone.ondragover = handleOver;
       basketTargetZone.ondragenter = handleEnter;
@@ -43,14 +60,35 @@ function App() {
     
   }, []);
 
+  let x = 1;
+
   return (
     <div className="App">
       <div className="single-selected-item">
+        <p>Тёс (25мм) </p>
         <img className="selected-item-img" src={board} alt="" draggable="true"/>
+        <label htmlFor="width">Выбрать ширину (мм):</label>
+        <select id='width' name="width" size={x}>
+          <option value="100">100</option>
+          <option value="120">120</option>
+          <option value="150">150</option>
+          <option value="180">180</option>
+          <option value="200">200</option>
+        </select>
+        <label htmlFor="length">Выбрать длину (метры):</label>
+        <select id='length' name="length" size={x}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="6">6</option>
+        </select>
+        <label htmlFor="quantity">Количество:</label>
+        <input type="number" id="quantity" name="quantity" min="1" ></input>
       </div>
-      <img src={basket} alt='bb' className="basket" useMap="#basket-map"/>
-      <map id="basket-map" name="basket-map">
-        <area shape="rect" coords="0,0,250,200" alt="" href='' />
+      <img src={basket} alt='' className="basket" useMap="#basket-map"/>
+      <map  name="basket-map">
+        <area className="basketTargetZone" shape="rect" coords="0,0,250,150" alt=''/>
       </map>
     </div>
   );
